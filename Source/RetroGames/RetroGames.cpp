dec0a2adb27da4a203bbb2728c2a6d782305fbad
Luccasdam <luccas_crims@hotmail.com>
2023-02-14 15:58:50 -0300

    00 - Initial Project Setup
    + Created a Basic MainMenu
    + Created a Basic PauseMenu

---
+++ Source/RetroGames/RetroGames.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "RetroGames.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RetroGames, "RetroGames" );
